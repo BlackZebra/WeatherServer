f344bf689e8ac5d927346d68a463b0b29204d898
Ivan Kostrulev <Wolf22222222@gmail.com>
2018-09-18 15:49:48 +0300

    Test

--- WeatherDemon/Program.cs
+++ WeatherDemon/Program.cs
@@ -10,8 +10,14 @@ namespace WeatherDemon
     {
         static void Main(string[] args)
         {
+            string env = "LISTEN_PORT";
+
+            Console.WriteLine(Environment.GetEnvironmentVariable(env,EnvironmentVariableTarget.Process));
+            Environment.SetEnvironmentVariable(env,)
             // Start the server  
             HttpHelper.StartServer();
+
+            //Environment.SetEnvironmentVariable[]
             HttpHelper.Listen(); // Start listening.  
         }
     }
